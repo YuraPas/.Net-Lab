bc01cb19c1a82a5d0ba9dfd388c4ba825492ba6d
Yuriy <yuriy.paslavsky@gmail.com>
2019-02-03 05:00:01 +0300

    First task done

--- TestProject.TaskLibrary/Tasks/Lesson1/Task1.cs
+++ TestProject.TaskLibrary/Tasks/Lesson1/Task1.cs
@@ -7,9 +7,50 @@ namespace TestProject.TaskLibrary.Tasks.Lesson1
 {
     public class Task1 : IRunnable
     {
+
+        class Rectangle
+        {
+            public double _xPointLeftFirst;
+            public double _xPointLeftSecond;
+             
+            public double _yPointLeftFirst;
+            public double _yPointLeftSecond;
+
+            public Rectangle(double xPointLeftFirst, double yPointLeftFirst, double xPointLeftSecond, double yPointLeftSecond)
+            {
+                _xPointLeftFirst = xPointLeftFirst;
+                _yPointLeftFirst = yPointLeftFirst;
+                _xPointLeftSecond = xPointLeftSecond;
+                _yPointLeftSecond = yPointLeftSecond;
+
+            }
+
+           
+            public  double CalculatePerimeter(double xPointLeftFirst, double yPointLeftFirst, double xPointLeftSecond, double yPointLeftSecond)
+            {
+                return 2 * ((xPointLeftSecond - xPointLeftFirst) + (yPointLeftSecond - yPointLeftFirst));
+            }
+
+            public double CalculateSquare(double xPointLeftFirst, double yPointLeftFirst, double xPointLeftSecond, double yPointLeftSecond)
+            {
+                return (xPointLeftSecond - xPointLeftFirst) * (yPointLeftSecond - yPointLeftFirst);
+            }
+        }
+
+
         public void Run()
         {
-            Console.WriteLine ("Hello, world!");
+            Rectangle rectInstance = new Rectangle(4, 2, 10, 0);
+            Console.Write("Rectangle perimether equals: ");
+            Console.WriteLine(rectInstance.CalculatePerimeter(rectInstance._xPointLeftFirst, rectInstance._yPointLeftFirst, rectInstance._xPointLeftSecond, rectInstance._yPointLeftSecond));
+
+
+            Console.Write("Rectangle square equals: ");
+            Console.WriteLine(rectInstance.CalculateSquare(rectInstance._xPointLeftFirst, rectInstance._yPointLeftFirst, rectInstance._xPointLeftSecond, rectInstance._yPointLeftSecond));
+
         }
+
+
+
     }
 }
