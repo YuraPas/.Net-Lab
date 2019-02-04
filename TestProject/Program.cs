62c18956c4274cc21dcb132a920b9a9bbaf23d95
Yuriy <yuriy.paslavsky@gmail.com>
2019-02-04 03:32:30 +0300

    initial commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List of .NetLab Tasks
+            var tasks = new IRunnable[]
+            {
+                new TaskLibrary.Tasks.Lesson1.Task1(),
+                //new TaskLibrary.Tasks.Lesson1.Task2(),
+                //new TaskLibrary.Tasks.Lesson2.Task1(),             
+            };
+
+            foreach (var task in tasks)
+            {
+                task.Run();
+            }
+            Console.ReadLine();
+        }
+    }
+}
