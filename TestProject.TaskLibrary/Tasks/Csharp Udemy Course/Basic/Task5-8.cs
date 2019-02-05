a3c7e8856ca8716c8109799dc541506c6c789732
Yuriy <yuriy.paslavsky@gmail.com>
2019-02-06 00:00:03 +0300

    Solving task from Udemy course

---
+++ TestProject.TaskLibrary/Tasks/Csharp Udemy Course/Basic/Task5-8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Csharp_Udemy_Course.Basic
+{
+    class Task5_8 : IRunnable
+    {
+        public void Run()
+        {
+            // #5
+            
+
+
+            // #6 
+
+
+
+            // #7 
+
+
+
+            // #8
+
+
+
+
+        }
+    }
+}
