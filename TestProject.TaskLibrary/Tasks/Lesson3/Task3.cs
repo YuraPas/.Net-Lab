8be127d5ac155299b34e34b7eb08c27df6f69841
Yuriy <yuriy.paslavsky@gmail.com>
2019-02-05 21:56:06 +0300

    Created skeleton for Lesson 3

---
+++ TestProject.TaskLibrary/Tasks/Lesson3/Task3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson3
+{
+    public class Task3 : IRunnable
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestProject.TaskLibrary/Tasks/Lesson3/Task4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson3
+{
+    public class Task4 : IRunnable
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
